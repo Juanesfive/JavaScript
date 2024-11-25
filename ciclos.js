ee12d9a5991e48bf3c85b4c704cb174928c9911f
Juan <juanes29061215@gmail.com>
2024-11-25 11:38:19 -0500

    avance de 25-11-24

--- ciclos.js
+++ ciclos.js
@@ -8,28 +8,55 @@
 // (===) Igual a y de mismo tipo
 // (!=) Diferente de
 
-let n = 8;
-let contador = 1 ;
+// let n = 8;
+// let contador = 1;
 // while (contador <= n) {
 //     console.log(contador);
 //     contador++;
-    
-    
-// }
 
+// }
 
 // do {
 //     console.log("?");
 
 // } while (false) {
-    
+
+// }
+// let numero = 10;
+
+// const esPrimo = (a) => {
+//   let primo = true;
+//   for (let i = 2; i <= a / 2; i++) {
+//     if (a % i === 0) primo = false;
+//   }
+//   return primo;
+// };
+// const llenado = function (numero) {
+//   console.log(numero);
+// };
+
+// llenado();
+
+//     let resp = esPrimo(numero);
+//     let primo = resp ? "es primo" : "no es primo";
+
+//     console.log(`el numero ${numero} es primo? ${primo}`);
+
+// for (let i = 0; i < 10; i++) {
+//   if (i === 4) {
+//     break;
+//   }
+//   console.log(`Item ${i}`);
 // }
 
-for (let i = 2; i<=40; i+=2) {
+// const numero = [1, 2, 3, 4,85];
+// let a = numero[0];
+// let b = numero[1];
+// let c = numero[2];
+// let d = numero[3];
 
-        console.log(i); 
-    
-}
+// let [a,b,c,d,e = 25] = numero;
+// console.log(a,b,c,d,e);
 
 
 
